6fdd12dcb8a4c1d5c8d9102d9f3011d8d31948d8
emmad-omnius <emmad@omnius.com>
2023-11-15 09:09:01 +0500

    user service login unit tests

--- src/user/test/user-service.spec.ts
+++ src/user/test/user-service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, HttpException } from '@nestjs/common';
+import { BadRequestException, HttpException, UnauthorizedException } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
@@ -8,7 +8,8 @@ import { Repository } from 'typeorm';
 import { UserEntity } from '../user.entity';
 import { UserRO } from '../user.interface';
 import { UserService } from '../user.service';
-import { userAuthRegisterDtoStub, userEntityStub, userROStub, userStub } from './stubs/user.stub';
+import { userAuthLoginDtoStub, userAuthRegisterDtoStub, userEntityStub, userROStub, userStub } from './stubs/user.stub';
+import * as argon from 'argon2';
 
 describe('UserService', () => {
     let userService: UserService;
@@ -67,7 +68,6 @@ describe('UserService', () => {
             });
 
             it('should call findOne of repository', async () => {
-                // jest.spyOn(userRepository, 'findOne');
                 await userService.registerUser(userAuthRegisterDtoStub());
                 expect(userRepository.findOne).toHaveBeenCalled();
             });
@@ -100,6 +100,65 @@ describe('UserService', () => {
                 const register = userService.registerUser(userAuthRegisterDtoStub());
                 await expect(register).rejects.toThrowErrorObject({
                     message: 'Username or email already exists',
+                });
+            });
+        });
+    });
+
+    describe('login', () => {
+        beforeAll(() => {
+            jest.spyOn(argon, 'verify').mockImplementation((hash: string, password: string) => {
+                if (hash === password) {
+                    return Promise.resolve(true);
+                }
+                return Promise.resolve(false);
+            });
+        });
+
+        beforeEach(() => {
+            jest.spyOn(userRepository, 'findOne').mockResolvedValue(userEntityStub());
+        });
+
+        describe('when login is called', () => {
+            it('should return proper object on success', async () => {
+                const userRO: UserRO = userROStub();
+                userRO.user.token = 'jwt token';
+
+                const user: UserRO = await userService.loginUser(userAuthLoginDtoStub());
+
+                expect(user).toEqual(userRO);
+            });
+
+            it('should call findOne of repository', async () => {
+                await userService.loginUser(userAuthLoginDtoStub());
+                expect(userRepository.findOne).toHaveBeenCalled();
+            });
+
+            it('should verify password hash', async () => {
+                await userService.loginUser(userAuthLoginDtoStub());
+                expect(argon.verify).toHaveBeenCalled();
+                expect(argon.verify).toHaveBeenCalledWith(userEntityStub().password, userAuthLoginDtoStub().password);
+            });
+
+            it('should throw error if user dont exists', async () => {
+                jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(null);
+
+                const login = userService.loginUser(userAuthLoginDtoStub());
+
+                await expect(login).rejects.toThrow(UnauthorizedException);
+                await expect(login).rejects.toThrowErrorObject({
+                    message: 'Invalid email or password',
+                });
+            });
+
+            it("should throw error if password doesn't match", async () => {
+                const loginDto = userAuthLoginDtoStub();
+                loginDto.password = 'some worng password';
+                const login = userService.loginUser(loginDto);
+
+                await expect(login).rejects.toThrow(UnauthorizedException);
+                await expect(login).rejects.toThrowErrorObject({
+                    message: 'Invalid email or password',
                 });
             });
         });
